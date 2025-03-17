31a623521b3f3d7973776070f83d125319ecaf27
Patric Gekoski <PatricGekoski@protonmail.com>
2025-03-17 12:14:10 -0400

    Create mobile image selector UI

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ImageSelectorComponent } from '../../components/image-selector/image-selector.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ImageSelectorComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
