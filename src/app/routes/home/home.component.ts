407dd313acd2b8a4041ae02cbcecd729e94b8e7a
Patric Gekoski <PatricGekoski@protonmail.com>
2025-03-17 21:06:13 -0400

    Make page responsive

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ImageSelectorComponent } from '../../components/image-selector/image-selector.component';
 import { ProductInfoComponent } from '../../components/product-info/product-info.component';
+import { ProductSelectorComponent } from '../../components/product-selector/product-selector.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ImageSelectorComponent, ProductInfoComponent],
+  imports: [
+    ImageSelectorComponent,
+    ProductInfoComponent,
+    ProductSelectorComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
